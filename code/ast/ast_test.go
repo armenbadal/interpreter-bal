347e3397f1a4757248f2c404a1f2133d833ddbd4
Արմեն Բադալյան <armenbadal@gmail.com>
2026-05-04 13:51:50 +0400

    Երկու պարզ թեսթ

--- code/ast/ast_test.go
+++ code/ast/ast_test.go
@@ -4,14 +4,30 @@ import (
 	"testing"
 )
 
-func TestPrimitives(t *testing.T) {
+func TestLiterals(t *testing.T) {
+	n0 := &Number{Value: 42}
+	e0 := "  number:\n    value: 42\n"
+	if n0.String() != e0 {
+		t.Fail()
+	}
+
+	t0 := &Text{Value: "hello"}
+	e1 := "  text:\n    value: \"hello\"\n"
+	if t0.String() != e1 {
+		t.Fail()
+	}
+
+	b0 := &Boolean{Value: true}
+	e2 := "  boolean:\n    value: true\n"
+	if b0.String() != e2 {
+		t.Fail()
+	}
 }
 
-func TestArrayLiteral(t *testing.T) {
-}
-
-func TestUnary(t *testing.T) {
-}
-
-func TestLet(t *testing.T) {
+func TestVariable(t *testing.T) {
+	v0 := &Variable{Name: "x"}
+	e0 := "  variable:\n    name: 'x'\n"
+	if v0.String() != e0 {
+		t.Fail()
+	}
 }
